fcda02a2c916c7100f9cfa27b2df7d567a5981a7
rehemae <rehemaephraimnanda@gmail.com>
2022-02-16 20:54:39 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name: String = ("rehema ephraim")
+    println(name)
+    var age = 30
+    println(age)
+    var phoneNumber: String = ("0757485872")
+    println(phoneNumber)
+    var weight: Float = 50f
+    println(weight)
+    var uganda: Boolean = true
+    println(uganda)
+
+}
